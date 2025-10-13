a112aea7723b6c678f2f2e82bdfb1fe067c3ac78
Ankit Mahato <101471279+ankitmahato03@users.noreply.github.com>
2025-10-13 10:33:43 +0000

    privacy policy page add

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+import { PrivacyPolicy } from "@/components/pages/privacy_policy";
+
+const PrivacyPolicyPage = () => {
+  return (
+    <div>
+      <PrivacyPolicy />
+    </div>
+  );
+};
+
+export default PrivacyPolicyPage;
