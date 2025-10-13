7ee266318fc62cf74fbf7f7e9ab88857165e6e0f
Ankit Mahato <101471279+ankitmahato03@users.noreply.github.com>
2025-10-13 10:34:01 +0000

    terms and conditions page add

---
+++ app/terms-conditions/page.tsx
@@ -0,0 +1,13 @@
+import { TermsAndConditions } from "@/components/pages/terms-and-conditions";
+
+ const TermsAndConditionsPage = () => {
+
+return(
+    <TermsAndConditions/>
+
+);
+
+}
+
+
+export default TermsAndConditionsPage;
---
+++ components/pages/terms-and-conditions.tsx
@@ -0,0 +1,185 @@
+export const TermsAndConditions = () => {
+  return (
+    <section className="flex justify-center items-start py-8 px-4">
+      <div className="w-full max-w-6xl xl:max-w-7xl bg-black/5 rounded-md p-6">
+        <h1 className="text-2xl font-bold items-center">
+          Terms and Conditions
+        </h1>
+        <p>Last updated: June 10, 2024</p>
+        <span className="space-y-2">
+          <p>
+            {" "}
+            I acknowledge that all GMP information is provided solely for
+            educational purposes. I am aware that GMP does not constitute
+            investment advice. I understand that GMP represents market rumors.
+            My application decisions are not influenced solely by GMP; I
+            consider other factors. I confirm that I apply for IPOs only after
+            consulting my SEBI registered advisor. I take full responsibility
+            for any profits or losses resulting from my IPO investments. I
+            acknowledge that IPO Ji App/Team does not recommend subscribing to
+            or avoiding any IPO. I release IPO Ji App/Team from any claims or
+            blame for investment losses. I affirm that applying for this IPO is
+            my independent decision, and I am familiar with the company’s
+            financial status. I will submit IPO applications solely for myself
+            and my family’s accounts. I am accountable for any bids made on
+            third-party accounts. I will verify my application number in the
+            bids section of IPO Ji App before approving any UPI mandate. I am
+            aware that applications through third-party banks/UPI may be
+            rejected. I understand that HNI bids cannot be canceled; I may
+            switch to retail bids. I acknowledge that cancelling an HNI bid to
+            switch to retail will not be facilitated by the exchange. I take
+            responsibility for cancelling bids with blocked payments. I have
+            thoroughly read the RHP and possess comprehensive knowledge about
+            the company I intend to invest in.
+          </p>
+          <p>
+            {" "}
+            By using IPO Ji App, I agree to the above-stated terms and
+            conditions.{" "}
+          </p>
+          <span className="text-xl font-bold">Privacy Policy</span>
+          <p>
+            IPO Ji is built as a Free app. This SERVICE is provided by IPO Ji at
+            : IPO Ji is built as a Free app. This SERVICE is provided by IPO Ji
+            at no cost and is intended for use as is. This page is used to
+            inform visitors regarding our policies regarding the collection,
+            use, and disclosure of Personal Information if anyone decided to use
+            our Service. If you choose to use our Service, then you agree to the
+            collection and use of information in relation to this policy. The
+            Personal Information that we collect is used for providing and
+            improving the Service. We will not use or share your information
+            with anyone except as described in this Privacy Policy. The terms
+            used in this Privacy Policy have the same meanings as in our Terms
+            and Conditions, which are accessible at IPO Ji unless otherwise
+            defined in this Privacy Policy.
+          </p>
+          <span className="text-xl font-bold">
+            Information Collection and Use
+          </span>
+          <p>
+            {" "}
+            For a better experience, while using our Service, we may require you
+            to provide us with certain personally identifiable information. The
+            information that we request will be retained by us and used as
+            described in this privacy policy. The app does use third-party
+            services that may collect information used to identify you. Link to
+            the privacy policy of third-party service providers used by the app
+            Google Play Services AdMob Google Analytics for Firebase Firebase
+            Crashlytics Facebook
+          </p>
+          <span> Log Data</span>{" "}
+          <p>
+            We want to inform you that whenever you use our Service, in case of
+            an error in the app we collect data and information (through
+            third-party products) on your phone called Log Data. This Log Data
+            may include information such as your device Internet Protocol (“IP”)
+            address, device name, operating system version, the configuration of
+            the app when utilizing our Service, the time and date of your use of
+            the Service, and other statistics.
+          </p>
+          <span> Cookies</span>{" "}
+          <p>
+            Cookies are files with a small amount of data that are commonly used
+            as anonymous unique identifiers. These are sent to your browser from
+            the websites that you visit and are stored on your device's internal
+            memory. This Service does not use these “cookies” explicitly.
+            However, the app may use third party code and libraries that use
+            “cookies” to collect information and improve their services. You
+            have the option to either accept or refuse these cookies and know
+            when a cookie is being sent to your device. If you choose to refuse
+            our cookies, you may not be able to use some portions of this
+            Service.
+          </p>{" "}
+          <span>Service Providers</span>{" "}
+          <p>
+            We may employ third-party companies and individuals due to the
+            following reasons: To facilitate our Service; To provide the Service
+            on our behalf; To perform Service-related services; or To assist us
+            in analyzing how our Service is used. We want to inform users of
+            this Service that these third parties have access to your Personal
+            Information. The reason is to perform the tasks assigned to them on
+            our behalf. However, they are obligated not to disclose or use the
+            information for any other purpose.
+          </p>{" "}
+          <span className="text-xl font-bold">Security</span>{" "}
+          <p>
+            We value your trust in providing us with your Personal Information,
+            thus we are striving to use commercially acceptable means of
+            protecting it. But remember that no method of transmission over the
+            internet, or method of electronic storage is 100% secure and
+            reliable, and we cannot guarantee its absolute security.
+          </p>
+          <span> Links to Other Sites</span>{" "}
+          <p>
+            This Service may contain links to other sites. If you click on a
+            third-party link, you will be directed to that site. Note that these
+            external sites are not operated by us. Therefore, we strongly advise
+            you to review the Privacy Policy of these websites. We have no
+            control over and assume no responsibility for the content, privacy
+            policies, or practices of any third-party sites or services.
+          </p>{" "}
+          <span>Children's Privacy</span>{" "}
+          <p>
+            These Services do not address anyone under the age of 13. We do not
+            knowingly collect personally identifiable information from children
+            under 13 years of age. In the case we discover that a child under 13
+            has provided us with personal information, we immediately delete
+            this from our servers. If you are a parent or guardian and you are
+            aware that your child has provided us with personal information,
+            please contact us so that we will be able to do the necessary
+            actions.
+          </p>
+          <span className="text-xl font-bold">Liability Disclaimer</span>{" "}
+          <p>
+            YOU EXPRESSLY AGREE THAT THE USE OF THE WEBSITE OR APP IS AT YOUR
+            SOLE RISK. The information and materials on this platform may
+            contain typographical errors or inaccuracies. Any dated information
+            is published as of its date, and IPO Ji does not undertake any
+            obligation or responsibility to update or amend such information.
+            The information, products, and services on this platform are
+            provided on an "AS IS, WITH ALL FAULTS," "WHERE IS," and "WHERE
+            AVAILABLE" basis. IPO Ji does not warrant the accuracy,
+            completeness, or reliability of the information or services provided
+            on its website or app, and expressly disclaims all implied
+            warranties, including but not limited to warranties of title,
+            non-infringement, merchantability, or fitness for a particular
+            purpose. IPO Ji shall not be responsible for any loss or damage
+            resulting from the interception of any information or services by
+            third parties. IPO Ji cannot and does not guarantee the accuracy,
+            validity, timeliness, or completeness of any information or data
+            available on its platform. Neither IPO Ji, nor its affiliates,
+            directors, officers, employees, third-party vendors, associates,
+            partners, or sub-brokers shall be liable for any loss, damage, or
+            compensation arising from any failure or interruption of the
+            platform, acts or omissions of any third party involved in providing
+            the website or app, the inability to access or use the platform, or
+            any other cause beyond IPO Ji's control. By using this platform, you
+            assume all risks associated with errors, omissions, or data
+            transmission issues, and bear full responsibility for ensuring the
+            accuracy and suitability of the information provided, as well as for
+            maintaining any backups or procedures required for data
+            reconstruction or analysis. YOU AGREE THAT IPO Ji (including its
+            officers, directors, employees, affiliates, group companies, agents,
+            representatives, or subcontractors) SHALL NOT BE LIABLE—whether in
+            contract or tort—for any direct, special, indirect, consequential,
+            or incidental damages arising out of the use or inability to use its
+            website or app, even if IPO Ji has been advised of the possibility
+            of such damages. IPO Ji and its affiliates, officers, directors,
+            employees, and agents shall have no liability in tort, contract, or
+            otherwise to users or any third party, including but not limited to
+            damages caused by viruses affecting user devices, failures of
+            mechanical or electronic equipment, communication line disruptions,
+            unauthorized access or security breaches, operator errors, labor
+            disputes, or force majeure events. IPO Ji does not guarantee
+            continuous, uninterrupted, or secure access to its website or app.{" "}
+          </p>{" "}
+          <span className="text-xl font-bold">Contact Us</span>{" "}
+          <p>
+            If you have any questions or suggestions about our Privacy Policy,
+            do not hesitate to contact us at support@mydomail.com.com
+          </p>
+        </span>
+      </div>
+    </section>
+  );
+};
